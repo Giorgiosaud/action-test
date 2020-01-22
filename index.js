cb9fbe41c10e25f1feefd238cccaf6d9657515c1
Giorgiosaud <jsaud@modyo.com>
2020-01-21 22:49:19 -0300

    dix

--- index.js
+++ index.js
@@ -11,10 +11,8 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   const context = JSON.stringify(github.context, undefined, 2)
-  const github = JSON.stringify(github, undefined, 2)
   console.log(`The event payload: ${payload}`);
   console.log(`The github context: ${context}`);
-  console.log(`The github object: ${github}`);
 } catch (error) {
   core.setFailed(error.message);
 }
